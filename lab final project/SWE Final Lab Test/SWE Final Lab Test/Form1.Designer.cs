de77496dadbb7622f2e577e9231836642351673f
mahidulmoon <49182474+mahidulmoon@users.noreply.github.com>
2019-04-13 19:33:00 +0600

    Add files via upload

---
+++ lab final project/SWE Final Lab Test/SWE Final Lab Test/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace SWE_Final_Lab_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pidbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnamebox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pqabox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pxdbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.deletebox = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(453, 196);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.datagridclick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(542, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PID";
+            // 
+            // pidbox
+            // 
+            this.pidbox.Location = new System.Drawing.Point(591, 60);
+            this.pidbox.Name = "pidbox";
+            this.pidbox.Size = new System.Drawing.Size(228, 20);
+            this.pidbox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(525, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "PName";
+            // 
+            // pnamebox
+            // 
+            this.pnamebox.Location = new System.Drawing.Point(591, 95);
+            this.pnamebox.Name = "pnamebox";
+            this.pnamebox.Size = new System.Drawing.Size(228, 20);
+            this.pnamebox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(471, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "PQuantityAvailable";
+            // 
+            // pqabox
+            // 
+            this.pqabox.Location = new System.Drawing.Point(591, 136);
+            this.pqabox.Name = "pqabox";
+            this.pqabox.Size = new System.Drawing.Size(228, 20);
+            this.pqabox.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(502, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "PExpiryDate";
+            // 
+            // pxdbox
+            // 
+            this.pxdbox.Location = new System.Drawing.Point(591, 170);
+            this.pxdbox.Name = "pxdbox";
+            this.pxdbox.Size = new System.Drawing.Size(228, 20);
+            this.pxdbox.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 57);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(591, 210);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 50);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Insert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(717, 210);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 50);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // deletebox
+            // 
+            this.deletebox.Location = new System.Drawing.Point(125, 327);
+            this.deletebox.Name = "deletebox";
+            this.deletebox.Size = new System.Drawing.Size(199, 20);
+            this.deletebox.TabIndex = 12;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(249, 353);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(717, 286);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(102, 41);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Clear";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(125, 354);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "Search";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 446);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.deletebox);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pxdbox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pqabox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pnamebox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pidbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pidbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox pnamebox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox pqabox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox pxdbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox deletebox;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ lab final project/SWE Final Lab Test/SWE Final Lab Test/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SWE_Final_Lab_Test
+{
+
+    public partial class Form1 : Form
+    {
+        private SqlConnection conn = new SqlConnection();
+        private string ConString = "Server=DESKTOP-HFA36BB; Database = ProductDB; User = sa; Password = 1114012833m";
+        private SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn.ConnectionString = ConString;
+            cmd = conn.CreateCommand();
+
+            try
+            {
+                string query = "sp_showall";
+                cmd.CommandText = query;
+                cmd.CommandType = CommandType.StoredProcedure;
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+
+                dataGridView1.DataSource = dt;
+
+                reader.Close();
+
+            }
+            catch (Exception ex)
+            {
+                string msg = ex.Message.ToString();
+                string caption = "Error";
+                MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cmd.Dispose();
+                conn.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1_Load(this, e);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string pid = pidbox.Text;
+            string pname = pnamebox.Text;
+            string pqa = pqabox.Text;
+            string ped = pxdbox.Text;
+
+
+
+            if ((pid == "") || (pname == "") || (pqa == "") || (ped == ""))
+            {
+                string msg = "No textbox can be empty";
+                string caption = "Error";
+                MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                
+                cmd = conn.CreateCommand();
+
+                try
+                {
+
+                    string query = "Insert into ProductInformation values(@pid,@pname,@pqa,@ped)";
+
+                    cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.Add("@pid", SqlDbType.VarChar);
+                    cmd.Parameters["@pid"].Value = pid;
+                    cmd.Parameters.Add("@pname", SqlDbType.VarChar);
+                    cmd.Parameters["@pname"].Value = pname;
+                    cmd.Parameters.Add("@pqa", SqlDbType.VarChar);
+                    cmd.Parameters["@pqa"].Value = pqa;
+                    cmd.Parameters.Add("@ped", SqlDbType.VarChar);
+                    cmd.Parameters["@ped"].Value = ped;
+
+
+                    cmd.CommandText = query;
+                    conn.Open();
+                    cmd.ExecuteScalar();
+
+
+                }
+                catch (Exception e1)
+                {
+                    string msg = e1.Message.ToString();
+                    string caption = "Error";
+                    MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    cmd.Dispose();
+                    conn.Close();
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(deletebox.Text);
+            if (id != 0)
+            {
+                cmd = conn.CreateCommand();
+                try
+                {
+
+                    string command = "sp_delete";
+                    cmd = new SqlCommand(command, conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@id", SqlDbType.Int);
+                    cmd.Parameters["@id"].Value = id;
+                    conn.Open();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    MessageBox.Show("Show Delete Result");
+
+                    reader.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    string msg = ex.Message.ToString();
+                    string caption = "Error";
+                    MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    cmd.Dispose();
+                    conn.Close();
+                }
+            }
+            else
+            {
+                string msg = "No textbox can be empty";
+                string caption = "Error";
+                MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void datagridclick(object sender, DataGridViewCellEventArgs e)
+        {
+           pidbox.Text= dataGridView1.CurrentRow.Cells[0].Value.ToString();
+           pnamebox.Text= dataGridView1.CurrentRow.Cells[1].Value.ToString();
+           pqabox.Text=dataGridView1.CurrentRow.Cells[2].Value.ToString();
+           pxdbox.Text= dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            string pname = pnamebox.Text;
+            string pqa = pqabox.Text;
+            string ped = pxdbox.Text;
+            string pid = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            int pid2 = Convert.ToInt32(pid);
+            cmd = conn.CreateCommand();
+
+            try
+            {
+
+                string query = "UPDATE ProductInformation SET PID=@pid2,PName = @pname, PQuantityAvailable = @pqa, PExpiryDate = @ped WHERE PID = @pid2 ";
+
+                cmd = new SqlCommand(query, conn);
+                cmd.Parameters.Add("@pid2", SqlDbType.VarChar);
+                cmd.Parameters["@pid2"].Value = pid;
+                cmd.Parameters.Add("@pname", SqlDbType.VarChar);
+                cmd.Parameters["@pname"].Value = pname;
+                cmd.Parameters.Add("@pqa", SqlDbType.VarChar);
+                cmd.Parameters["@pqa"].Value = pqa;
+                cmd.Parameters.Add("@ped", SqlDbType.VarChar);
+                cmd.Parameters["@ped"].Value = ped;
+
+
+                cmd.CommandText = query;
+                conn.Open();
+                cmd.ExecuteScalar();
+
+
+            }
+            catch (Exception e1)
+            {
+                string msg = e1.Message.ToString();
+                string caption = "Error";
+                MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                cmd.Dispose();
+                conn.Close();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pidbox.Text = "";
+            pnamebox.Text = "";
+            pqabox.Text = "";
+            pxdbox.Text = "";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(deletebox.Text);
+            if (id != 0)
+            {
+                cmd = conn.CreateCommand();
+                try
+                {
+
+                    string command = "select * from ProductInformation where PID=@id";
+                    cmd = new SqlCommand(command, conn);
+
+                    cmd.Parameters.Add("@id", SqlDbType.Int);
+                    cmd.Parameters["@id"].Value = id;
+                    conn.Open();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    MessageBox.Show("Press OK to show result");
+                    
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
+
+                    dataGridView1.DataSource = dt;
+
+                    reader.Close();
+                    reader.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    string msg = ex.Message.ToString();
+                    string caption = "Error";
+                    MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    cmd.Dispose();
+                    conn.Close();
+                }
+            }
+            else
+            {
+                string msg = "No textbox can be empty";
+                string caption = "Error";
+                MessageBox.Show(msg, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
